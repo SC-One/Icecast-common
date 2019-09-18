81da38369a7378566e6600f129f7b55ab4a03078
Philipp Schafft <lion@lion.leolix.org>
2019-09-18 12:16:55 +0000

    Update: Do not allow using the default constructor for sockets

--- src/socket.c
+++ src/socket.c
@@ -11,25 +11,19 @@
 #endif
 
 #include <igloo/socket.h>
+#include <igloo/io.h>
 
 struct igloo_socket_tag {
     igloo_ro_base_t __base;
 };
 
-static int __new(igloo_ro_t self, const igloo_ro_type_t *type, va_list ap);
 static void __free(igloo_ro_t self);
 static igloo_ro_t __get_interface_t(igloo_ro_t self, const igloo_ro_type_t *type, const char *name, igloo_ro_t associated, igloo_ro_t instance);
 
 igloo_RO_PUBLIC_TYPE(igloo_socket_t,
         igloo_RO_TYPEDECL_FREE(__free),
-        igloo_RO_TYPEDECL_NEW(__new),
         igloo_RO_TYPEDECL_GET_INTERFACE(__get_interface_t)
         );
-
-static int __new(igloo_ro_t self, const igloo_ro_type_t *type, va_list ap)
-{
-    return 0;
-}
 
 static void __free(igloo_ro_t self)
 {
