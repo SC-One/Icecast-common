973fb3eaa4690d7fb2ce7e238f35a0646738785d
Philipp Schafft <lion@lion.leolix.org>
2019-10-02 06:27:16 +0000

    Update: Added some docs

--- include/igloo/socketaddr.h
+++ include/igloo/socketaddr.h
@@ -40,30 +40,55 @@ extern "C" {
 
 igloo_RO_FORWARD_TYPE(igloo_socketaddr_t);
 
+/* Domain of sockets */
 typedef enum {
+    /* Special value used for unspecified or not jet known domain. */
 	igloo_SOCKETADDR_DOMAIN_UNSPEC = 0,
+    /* Unix Domain Sockets, AKA AF_UNIX */
     igloo_SOCKETADDR_DOMAIN_UNIX,
+    /* IPv4 Sockets, AKA AF_INET */
     igloo_SOCKETADDR_DOMAIN_INET4,
+    /* IPv6 Sockets, AKA AF_INET6 */
     igloo_SOCKETADDR_DOMAIN_INET6
 } igloo_socketaddr_domain_t;
 
+/* Socket type */
 typedef enum {
+    /* Special value used for unspecified or not jet known type. */
 	igloo_SOCKETADDR_TYPE_UNSPEC = 0,
+    /* Stream sockets */
 	igloo_SOCKETADDR_TYPE_STREAM,
+    /* Datagram sockets */
 	igloo_SOCKETADDR_TYPE_DGRAM,
+    /* Sequenced and reliable datagram sockets */
 	igloo_SOCKETADDR_TYPE_SEQPACK,
+    /* Reliable datagram sockets */
     igloo_SOCKETADDR_TYPE_RDM
 } igloo_socketaddr_type_t;
 
+/* Specific socket protocol */
 typedef enum {
+    /* Special value used for unspecified or not jet known protocol. */
     igloo_SOCKETADDR_PROTOCOL_UNSPEC = 0,
+    /* Transmission Control Protocol */
     igloo_SOCKETADDR_PROTOCOL_TCP,
+    /* User Datagram Protocol */
     igloo_SOCKETADDR_PROTOCOL_UDP,
+    /* Datagram Congestion Control Protocol */
     igloo_SOCKETADDR_PROTOCOL_DCCP,
+    /* Stream Control Transmission Protocol */
     igloo_SOCKETADDR_PROTOCOL_SCTP,
+    /* Lightweight User Datagram Protocol */
     igloo_SOCKETADDR_PROTOCOL_UDPLITE
 } igloo_socketaddr_protocol_t;
 
+/* This creates a new address object.
+ * Parameters:
+ *  name, associated, instance
+ *      See igloo_ro_new().
+ *  domain, type, protocol
+ *      Domain, Type, and Protocol for the new address. Can be igloo_SOCKETADDR_*_UNSPEC.
+ */
 igloo_socketaddr_t *    igloo_socketaddr_new(igloo_socketaddr_domain_t domain, igloo_socketaddr_type_t type, igloo_socketaddr_protocol_t protocol, const char *name, igloo_ro_t associated, igloo_ro_t instance, igloo_error_t *error);
 igloo_error_t           igloo_socketaddr_get_base(igloo_socketaddr_t *addr, igloo_socketaddr_domain_t *domain, igloo_socketaddr_type_t *type, igloo_socketaddr_protocol_t *protocol);
 
